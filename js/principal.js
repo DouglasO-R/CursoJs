67e9bb1c0cb2bd594984b196f992d8a4da11f44d
Douglas O.R <douglasor@outlook.com>
2017-08-11 21:32:49 -0300

    fazendo validacao dos campos

--- js/principal.js
+++ js/principal.js
@@ -1,16 +1,36 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Nutricionista Aparecida";
 
-var trPaciente = document.querySelector("#primeiro-paciente");
-var peso = document.querySelector(".info-peso").textContent;
+var Paciente = document.querySelector("#primeiro-paciente");
+var peso = Paciente.querySelector(".info-peso").textContent;
 
 
-var altura = document.querySelector(".info-altura").textContent;
+var altura = Paciente.querySelector(".info-altura").textContent;
 
-
-var imc = peso / (altura * altura);
+var alturaV = true;
+var pesoV = true;
 var Tdimc = document.querySelector(".info-imc");
-Tdimc.textContent = imc;
+
+if(altura >= 3.00 || altura <= 0)
+    {
+        console.log("altura invalida")
+        alturaV = false;
+        Tdimc.textContent = "Altura inválida!";
+    }
+if(peso >= 100 || peso <= 0)
+    {
+        console.log("peso invalido");
+        pesoV = false;
+        Tdimc.textContent = "Peso inválido!";
+    }    
+ if(alturaV && pesoV)
+    {
+        var imc = peso / (altura * altura);       
+        Tdimc.textContent = imc;
+    }   
+
+
+
 
 console.log(peso);
 console.log(altura);
