81275ee46a364a9f6a6cc98660e340fccea8e9d1
Douglas O.R <douglasor@outlook.com>
2017-09-07 17:01:29 -0300

    conclusao do curso alura javascript

--- js/busca.js
+++ js/busca.js
@@ -3,19 +3,28 @@ var buscaPaciente = document.querySelector("#btnBusca");
 buscaPaciente.addEventListener("click",function () {
 
     var req = new XMLHttpRequest();
-    req.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    req.open("GET", "https://api-pacientes.herokuapp.com/paci11entes");
     
     req.addEventListener("load", function() {
-        
+        var erroAjax = document.querySelector("#erro-ajax");
+
+      if(req.status == 200) {
+
+        erroAjax.classList.add("invisivel");
         var result = req.responseText;
-        console.log(typeof result);
         var pacientes = JSON.parse(result);
-        console.log(pacientes);
 
         pacientes.forEach( function(paciente){
             adicionaTabela(paciente);
 
         });
+
+      }else{
+          
+        erroAjax.classList.remove("invisivel");
+
+      }
+
     });
 
     req.send();
